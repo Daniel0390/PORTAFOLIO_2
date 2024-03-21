7a6b423ae0a3ffe55bda29c98e24d208abf8a100
Jodaniel Eusebio <133549697+Daniel0390@users.noreply.github.com>
2024-03-21 16:15:36 -0400

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,35 @@
+
+
+const  btncontactos = document.getElementById("contactos");
+const  mifone = document.getElementById("telefono");
+const  btnskills = document.getElementById("skills");
+const  imagenes  = document.getElementsByTagName("img");
+
+      
+
+
+
+
+       btncontactos.addEventListener("click", () =>{
+           mifone.toggleAttribute("hidden");
+     })
+
+
+      btnskills.addEventListener("click" , ()=>{
+           
+      } )
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
